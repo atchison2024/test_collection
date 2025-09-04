f0d1614bf4666df0e4299cd29603dbcd9d8fdaef
Atchison 2025 <ye@atchison.com.au>
2025-09-04 15:18:10 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,70 @@
 import dash
-from dash import html
+from dash import dcc, html, Input, Output
 
 app = dash.Dash(__name__)
-server = app.server  # 🔴 VERY IMPORTANT for deployment
 
+# Questions for each borrower type
+questions = {
+    'Company / Trust': [
+        html.Label('Company Name:'),
+        dcc.Input(type='text', id='company-name'),
+
+        html.Label('ABN or ACN:'),
+        dcc.Input(type='text', id='abn'),
+
+        html.Label('Trustee Name (if trust):'),
+        dcc.Input(type='text', id='trustee-name'),
+    ],
+    'Individual': [
+        html.Label('Full Name:'),
+        dcc.Input(type='text', id='full-name'),
+
+        html.Label('Date of Birth:'),
+        dcc.Input(type='date', id='dob'),
+
+        html.Label('Driver’s Licence Number:'),
+        dcc.Input(type='text', id='licence'),
+    ],
+    'Partnership': [
+        html.Label('Partnership Name:'),
+        dcc.Input(type='text', id='partnership-name'),
+
+        html.Label('Names of All Partners:'),
+        dcc.Textarea(id='partners', placeholder='List all partners here'),
+
+        html.Label('Partnership ABN:'),
+        dcc.Input(type='text', id='partnership-abn'),
+    ]
+}
+
+# App Layout
 app.layout = html.Div([
-    html.H1("Hello from Dash on Render!")
+    html.H2('Borrower Information Questionnaire'),
+
+    html.Label('Select Borrower Type:'),
+    dcc.Dropdown(
+        id='borrower-type',
+        options=[
+            {'label': 'Company / Trust', 'value': 'Company / Trust'},
+            {'label': 'Individual', 'value': 'Individual'},
+            {'label': 'Partnership', 'value': 'Partnership'},
+        ],
+        placeholder="Select borrower type"
+    ),
+
+    html.Div(id='dynamic-questions')
 ])
 
+# Callback to render the questions based on borrower type
+@app.callback(
+    Output('dynamic-questions', 'children'),
+    Input('borrower-type', 'value')
+)
+def display_questions(borrower_type):
+    if borrower_type is None:
+        return html.Div("Please select a borrower type to view questions.")
+    return html.Div(questions[borrower_type])
+
+# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
